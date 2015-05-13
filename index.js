1db8d377be4b3e31bd40b12d1d7c5e8a28889e0b
morganherlocker <morgan.herlocker@gmail.com>
2015-05-13 19:48:57 -0400

    basic map display and bounds fit

--- index.js
+++ index.js
@@ -1,7 +1,12 @@
-var concat = require('concat-stream')
-var argv = require('minimist')(process.argv.slice(2))
-var fs = require('fs')
+#!/usr/bin/env node
+var argv = require('minimist')(process.argv.slice(2));
+var fs = require('fs');
+var concat = require('concat-stream');
 
-((argv._[0] && fs.createReadStream(argv._[0])) || process.stdin).pipe(concat(openData));
-
-console.log(argv)
+((argv._[0] && fs.createReadStream(argv._[0])) || process.stdin).pipe(concat(function(geo){
+  geo = geo.toString()
+  fs.readFile(__dirname+'/index.html', 'utf8', function(err, html){
+    html = html.split('{{geojson}}').join(geo)
+    console.log(html)
+  })
+}))
